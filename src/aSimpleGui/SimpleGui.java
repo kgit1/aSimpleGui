f5e6c23dc60a2b72574b7aa9b9d140564cc4420c
kgit1 <kgit1@rambler.ru>
2016-12-01 23:05:29 +0200

    button with action changing message

--- src/aSimpleGui/SimpleGui.java
+++ src/aSimpleGui/SimpleGui.java
@@ -1,32 +1,32 @@
 package aSimpleGui;
 
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class SimpleGui {
 
 	public static void main(String[] args) {
-		
-		//window
+		// window
 		JFrame frame = new JFrame();
-		//button
+		// button
 		JButton button = new JButton("click me");
-		
-		//will stop programm on window close
+		// will stop programm on window close
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//add content(button) to window
+
+		// add content(button) to window
 		frame.getContentPane().add(button);
-		
-		//set window size
+
+		// set window size
 		frame.setSize(300, 300);
-		
-		//make window visible
+
+		// make window visible
 		frame.setVisible(true);
 
 	}
-	
-	public void changeIt(JButton button){
+
+	public void changeIt(JButton button) {
 		button.setText("Ive been clicked");
 	}
 
