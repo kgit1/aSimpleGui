48d91119e1452d8c50817408c7b0955f94e080d1
kgit1 <kgit1@rambler.ru>
2016-12-01 22:44:44 +0200

    on

--- src/aSimpleGui/SimpleGui.java
+++ src/aSimpleGui/SimpleGui.java
@@ -1,9 +1,13 @@
 package aSimpleGui;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class SimpleGui {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		JButton button = new JButton("click me");
 
 	}
 
